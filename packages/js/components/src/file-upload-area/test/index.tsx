bcabeadc80766d3a8525d7fa67a216bfa7655289
Fernando Marichal <contacto@fernandomarichal.com>
2022-11-01 13:12:46 -0300

    Fix tests

--- packages/js/components/src/file-upload-area/test/index.tsx
+++ packages/js/components/src/file-upload-area/test/index.tsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React, { createElement } from '@wordpress/element';
 
 /**
@@ -10,31 +9,10 @@ import React, { createElement } from '@wordpress/element';
  */
 import FileUploadArea from '../file-upload-area';
 
-/**
- * Browser dependencies
- */
-const { File } = window;
-
-// @testing-library/user-event considers changing <input type="file"> to a string as a change, but it do not occur on real browsers, so the comparisons will be against this result
-const fakePath = expect.objectContaining( {
-	target: expect.objectContaining( {
-		value: 'C:\\fakepath\\hello.png',
-	} ),
-} );
-
 describe( 'FileUploadArea', () => {
-	beforeEach( () => {
-		jest.useFakeTimers();
-	} );
-
-	afterEach( () => {
-		jest.runOnlyPendingTimers();
-		jest.useRealTimers();
-	} );
-
 	it( 'should show a button and a hidden input', () => {
 		render(
-			<FileUploadArea onUpload={ () => {} }>
+			<FileUploadArea>
 				<button>My Upload Button</button>
 			</FileUploadArea>
 		);
@@ -43,62 +21,5 @@ describe( 'FileUploadArea', () => {
 		const input = screen.getByTestId( 'form-file-upload-input' );
 		expect( button ).toBeInTheDocument();
 		expect( input ).toHaveStyle( 'display: none' );
-	} );
-
-	it( 'should not fire a change event after selecting the same file', async () => {
-		const user = userEvent.setup( {
-			advanceTimers: jest.advanceTimersByTime,
-		} );
-
-		const onChange = jest.fn();
-
-		render(
-			<FileUploadArea onUpload={ onChange }>
-				My Upload Button
-			</FileUploadArea>
-		);
-
-		const file = new File( [ 'hello' ], 'hello.png', {
-			type: 'image/png',
-		} );
-
-		const input = screen.getByTestId( 'form-file-upload-input' );
-
-		await user.upload( input, file );
-
-		await user.upload( input, file );
-
-		expect( onChange ).toHaveBeenCalledTimes( 1 );
-		expect( onChange ).toHaveBeenCalledWith( fakePath );
-	} );
-
-	it( 'should fire a change event after selecting the same file if the value was reset in between', async () => {
-		const user = userEvent.setup( {
-			advanceTimers: jest.advanceTimersByTime,
-		} );
-
-		const onChange = jest.fn();
-
-		render(
-			<FileUploadArea
-				onClick={ jest.fn( ( e ) => ( e.currentTarget.value = '' ) ) }
-				onChange={ onChange }
-			>
-				My Upload Button
-			</FileUploadArea>
-		);
-
-		const file = new File( [ 'hello' ], 'hello.png', {
-			type: 'image/png',
-		} );
-
-		const input = screen.getByTestId( 'form-file-upload-input' );
-		await user.upload( input, file );
-
-		expect( onChange ).toHaveBeenNthCalledWith( 1, fakePath );
-
-		await user.upload( input, file );
-
-		expect( onChange ).toHaveBeenNthCalledWith( 2, fakePath );
 	} );
 } );
